b3cece327add573079e98c471e15ab951bc46121
ArsPro13 <Vintbolt13@gmail.com>
2025-04-07 01:46:22 +0300

    fix app

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from app import create_app
 
-app = Flask(__name__)
+app = create_app()
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    with app.app_context():
+        from app.extensions import db
+        db.create_all()
+
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
