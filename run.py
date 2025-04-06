30e76292c36e33f48eb71ddb9b94c0ed0fc52354
ArsPro13 <Vintbolt13@gmail.com>
2025-04-07 01:43:17 +0300

    fix app

--- run.py
+++ run.py
@@ -7,4 +7,4 @@ def login():
     return "Hello from /login over HTTPS!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, ssl_context=("certs/cert.pem", "certs/key.pem"))
+    app.run(host="0.0.0.0", port=5000)
