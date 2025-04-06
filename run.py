e5acc644238b5e50285a7dfc624166aed8dc894b
ArsPro13 <Vintbolt13@gmail.com>
2025-04-07 01:45:56 +0300

    fix app

--- run.py
+++ run.py
@@ -2,9 +2,5 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/login', methods=['GET'])
-def login():
-    return "Hello from /login over HTTPS!"
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
