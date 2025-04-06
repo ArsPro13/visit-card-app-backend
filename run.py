bdab52e4d0a09e2b6fe8a7052c73478ff6358f6d
ArsPro13 <Vintbolt13@gmail.com>
2025-04-07 01:36:08 +0300

    fix app

--- run.py
+++ run.py
@@ -1,11 +1,10 @@
-from app import create_app
+from flask import Flask
 
-app = create_app()
+app = Flask(__name__)
 
-if __name__ == '__main__':
-    with app.app_context():
-        from app.extensions import db
-        db.create_all()
+@app.route('/login', methods=['GET'])
+def login():
+    return "Hello from /login over HTTPS!"
 
-    app.run(host='0.0.0.0', port=5000, debug=False, ssl_context=('certs/cert.pem', 'certs/key.pem'))
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, ssl_context=("certs/cert.pem", "certs/key.pem"))
