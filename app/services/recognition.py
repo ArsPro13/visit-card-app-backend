8810d505b8c83e9562ebcf086413151136107b93
ArsPro13 <Vintbolt13@gmail.com>
2025-04-07 10:17:16 +0300

    change model

--- app/services/recognition.py
+++ app/services/recognition.py
@@ -9,12 +9,12 @@ def process_recognizing_image(image):
     return {
         'name': mocked_cards[random_number]["name"],
         'job': mocked_cards[random_number]["job"],
-        'company_name': mocked_cards[random_number]["company_name"],
+        'companyName': mocked_cards[random_number]["company_name"],
         'phones': mocked_cards[random_number]["phones"],
         'email': mocked_cards[random_number]["email"],
         'address': mocked_cards[random_number]["address"],
         'websites': mocked_cards[random_number]["websites"],
-        'social_medias': mocked_cards[random_number]["social_medias"],
+        'socialMedias': mocked_cards[random_number]["social_medias"],
         'competencies': mocked_cards[random_number]["competencies"],
     }
 
